5578c9043ff813eb442b72286a8aed03b9c8187d
Игорь Соколов <sokoligor2000@gmail.com>
2025-03-04 20:10:28 +0500

    complete profile page

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import api from "../../axiosConfig";
+import axios from "axios";
 
 export const login = async (email: string, password: string) => {
   try {
@@ -37,4 +38,67 @@ export const getInfo = async () => {
     throw new Error("GetInfo failed");
   }
 }
+
+export const getUserName = async () => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    console.error("Token not found in localStorage");
+    return;
+}
+
+try {
+    const response = await api.get(`/profile?token=${token}`);
+    if (response.data.success) {
+        return response.data;
+    }
+    return response.data;
+} catch (error) {
+    console.error("GetUserInfo failed", error);
+    throw new Error("GetUserInfo failed");
+}
+}
+
+export const getAuthor = async (signal?: AbortSignal) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    console.error("Token not found in localStorage");
+    return;
+  }
+
+  try {
+    const response = await api.get(`/author?token=${token}`, { signal });
+    if (response.data.success) {
+      return response.data;
+    }
+    return response.data;
+  } catch (error) {
+    if (axios.isCancel(error)) {
+      console.warn("GetAuthor request cancelled");
+    } else {
+      console.error("GetAuthor failed", error);
+    }
+    throw new Error("GetAuthor failed");
+  }
+};
+
+export const getQuote = async (authorId: number, signal?: AbortSignal) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    console.error("Token not found in localStorage");
+    return;
+  }
+
+  try {
+    const response = await api.get(`/quote?token=${token}&authorId=${authorId}`, { signal });
+    return response.data;
+  } catch (error) {
+    if (axios.isCancel(error)) {
+      console.warn("GetQuote request cancelled");
+    } else {
+      console.error("GetQuote failed", error);
+    }
+    throw new Error("GetQuote failed");
+  }
+};
+
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,81 @@
+import { Avatar, Button, Modal } from "antd";
+import { getAuthor, getQuote, getUserName } from "../api/api";
+import { useEffect, useState } from "react";
+
 const Profile = () => {
+  const [userName, setUserName] = useState("Username");
+  const [authorName, setAuthorName] = useState("Author");
+  const [quote, setQuote] = useState("Quote of the day is...");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [step1Status, setStep1Status] = useState("Requesting author...");
+  const [step2Status, setStep2Status] = useState("Requesting quote...");
+  const [controller, setController] = useState<AbortController | null>(null);
+
+  const handleUpdate = async () => {
+    setIsModalOpen(true);
+    const abortController = new AbortController();
+    setController(abortController);
+    try {
+      const responseAuthor = await getAuthor(abortController.signal);
+      if (responseAuthor?.success) {
+        setAuthorName(responseAuthor.data.name);
+        setStep1Status("Requesting author... completed");
+      } else {
+        console.error("GetAuthor failed: ", responseAuthor?.data?.message);
+      }
+
+      const responseQuote = await getQuote(responseAuthor.data.authorId, abortController.signal);
+      if (responseQuote?.success) {
+        setQuote(responseQuote.data.quote);
+        setStep2Status("Requesting quote... completed");
+      } else {
+        console.error("GetQuote failed: ", responseQuote?.data?.message);
+      }
+    } catch (error) {
+      console.error("Request canceled or failed: ", error);
+    }
+  };
+
+  const handleCancel = () => {
+    if (controller) {
+      controller.abort();
+      setStep1Status("Requesting author... canceled");
+      setStep2Status("Requesting quote... canceled");
+    }
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getUserName();
+        if (response?.success) {
+          setUserName(response.data.fullname);
+        } else {
+          console.error("GetUserInfo failed: ", response?.data?.message);
+        }
+      } catch (error) {
+        console.error("GetUserInfo error: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h2>Profile</h2>
+      <Avatar src={"/assets/avatar.webp"} size={150} />
+      <div>
+        <h2>Welcome, {userName}!</h2>
+        <Button onClick={handleUpdate}>Update</Button>
+      </div>
+      <p>{authorName}: {quote}</p>
+
+      <Modal title="Requesting the quote" open={isModalOpen} onCancel={handleCancel} footer={null}>
+        <p>{step1Status}</p>
+        <p>{step2Status}</p>
+        <Button onClick={handleCancel}>Cancel</Button>
+      </Modal>
     </div>
   );
 };
